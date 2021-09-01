b0ca000acdb40ede42c975bf4d45f82e53207c05
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2021-08-31 23:56:33 -0400

    implemented a player chips feature that rewards the player for winning and punished them for losing.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-isAlive = false
-player = {
-    name : "Vincent",
-    chips : 123
-}
-messageEl = document.getElementById("message-el")
-cardsEl = document.getElementById("cards-el")
-sumEl = document.getElementById("sum-el")
-playerEl = document.getElementById("player-el")
+let isAlive = false
+let aliveOnCurrent = false
+let numChips = 50
+
+const CURRENTLY_RUNNING = "Do you want to draw a card?"
+const WON = "You got blackjack!"
+const LOST = "You lost"
+
+const messageEl = document.getElementById("message-el")
+const cardsEl = document.getElementById("cards-el")
+const sumEl = document.getElementById("sum-el")
+const playerEl = document.getElementById("player-el")
+const gameStart = document.getElementById("start-blackjack")
 
 function createRandomCard() {
     rand = Math.floor(Math.random() * 13 + 1);
@@ -20,9 +24,42 @@ function createRandomCard() {
     
 }
 
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (i = 0; i < cards.length; i += 1) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        state = CURRENTLY_RUNNING
+    } else if (sum === 21) {
+        state = WON
+        hasBlackJack = true
+        numChips += 20
+    } else {
+        state = LOST
+        aliveOnCurrent = false
+        numChips -= 10
+    }
+    checkChips()
+    playerEl.innerHTML = "Your Chips: " + numChips
+    messageEl.innerHTML = state
+    console.log(state)
+}
+
+// starts the Game when you first enter the website, as well as 
+// initializing the number of chips back to 50 if you were to restart
 function startGame() {
+    numChips = 50
+    startBlackjack()
+    gameStart.innerHTML = "START"
+    gameStart.onclick = startBlackjack
+}
+
+// starts a single round of Blackjack
+function startBlackjack() {
     hasBlackJack = false
-    isAlive = true
+    aliveOnCurrent = true
     firstCard = createRandomCard()
     secondCard = createRandomCard()
     cards = [firstCard, secondCard]
@@ -31,30 +68,20 @@ function startGame() {
     renderGame()
 }
 
-function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (i = 0; i < cards.length; i += 1) {
-        cardsEl.textContent += cards[i] + " "
-    }
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You got black jack!"
-        hasBlackJack = true
-    } else {
-        message = "You lost"
-        isAlive = false
-    }
-    playerEl.innerHTML = player.name + ": $" + player.chips 
-    messageEl.innerHTML = message
-}
-
 function drawCard() {
-    if (isAlive && !hasBlackJack) {
+    if (aliveOnCurrent && !hasBlackJack) {
         newCard = createRandomCard()
         cards.push(newCard)
         sum += newCard
         renderGame()
+    }
+}
+
+function checkChips() {
+    if (numChips <= 0) {
+        isAlive = false
+        console.log("LOST!!!")
+        gameStart.innerHTML = "RETRY?"
+        gameStart.onclick = startGame
     }
 }
